445c7e905709752aca44b8d9ea5a442f408add18
Akhi1Babu <akhilb732@gmail.com>
2025-06-24 17:30:06 +0530

    Added forums

--- lib/new_item.dart
+++ lib/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list_app/data/categories.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -13,9 +14,74 @@ class _NewItemState extends State<NewItem> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add New Item'),
-      
       ),
-      body: Padding(padding: EdgeInsets.all(16)),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Form(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Item Name',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter an item name';
+                }
+                return null;
+              },
+            ),
+            SizedBox(height: 20,),
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Quantity',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    initialValue: "1",
+                    keyboardType: TextInputType.number,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a quantity';
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                    child: DropdownButtonFormField(items: [
+                  for (var category in categories.entries)
+                    DropdownMenuItem(
+                      value: category.value,
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 15,
+                            height: 15,
+                            color: category.value.color,
+                          ),
+                          SizedBox(width: 8),
+                          Text(category.value.name),
+                        ],
+                      ),
+                    ),
+                ], onChanged: (value) {}))
+              ],
+            )
+          ],
+        )),
+      ),
     );
   }
 }
